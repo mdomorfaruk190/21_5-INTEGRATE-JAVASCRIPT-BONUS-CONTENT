8a3963b24df90322e8ba04f47a9b052641a1bcd0
md.omorFaruksoft90 <mdomorfaruk90g1@gmail.com>
2025-10-07 13:21:17 +0600

    Added complex caluator in MultipleNumber.js

--- MultipleNumber.js
+++ MultipleNumber.js
@@ -1,4 +1,4 @@
-const expression = "394+23-34*4/4";
+const expression = "394+23-34*4/4+10";
 // const expressionToArray = expression.split(/[\+\-\*\/]/);
 
 let temp = [];
@@ -15,7 +15,6 @@ for (let i = 0; i < expression.length; i++){
         let currentStringNumber = '';
 
         for (let j = start; j < i; j++) {
-            console.log('under j: ', expression[j]);
             currentStringNumber = currentStringNumber + expression[j];
         }
 
@@ -29,8 +28,112 @@ for (let i = 0; i < expression.length; i++){
 }
 temp[temp.length ] = lastNumber;
 
+let newArray = [...temp];
 
 for (let i = 0; i < temp.length; i++) {
-    const element = temp[i];
-    if(element )
+    const element = newArray[i];
+    if(element === "/"){
+        const result = getResult(i, element);
+        updateArray(i, result);
+        i =0;
+    }
+    if(element === "*"){
+        const result = getResult(i, element);
+       updateArray(i, result);
+        i = 0;
+    }
 }
+
+for (let i = 0; i < newArray.length; i++){
+    const element = newArray[i];
+    if (element === "+"){
+        const result = getResult(i, element)
+        updateArray(i, result);
+        i = 0;
+    }
+    if( element === '-'){
+        const result = getResult(i, element);
+        updateArray(i, result);
+        i = 0;
+    }
+}
+
+console.log(newArray.toString());
+
+function updateArray(elementIndex, result){
+    const newPositionForResult = elementIndex - 1;
+
+    newArray.splice(newPositionForResult, 3, result);
+}
+
+function getResult(elementIndex, element){
+    const leftOperant = newArray[elementIndex - 1];
+    const rightOperant = newArray[elementIndex + 1];
+    let result;
+    switch (element){
+        case "+":
+            result = parseFloat(leftOperant) + parseFloat(rightOperant);
+            break;
+        case "-":
+            result = parseFloat(leftOperant) -  parseFloat(rightOperant);
+            break;
+        case "*":
+            result = parseFloat(leftOperant) *  parseFloat(rightOperant);
+            break;
+        case "/":
+            result = parseFloat(leftOperant) /  parseFloat(rightOperant);
+            break;
+        default:
+            result = 0;
+            break;
+    }
+    return result;
+}
+
+
+
+/* [
+  '394', '+', '23',
+  '-',   34,  '+',
+  '10'
+] calculation this
+for (let i = 0; i < temp.length; i++) {
+    const element = newArray[i];
+    if(element === "/"){
+        const previousStringNumber = newArray[i -1];
+        const nextStringNumber = newArray[ i + 1];
+        const result = parseFloat(previousStringNumber) / parseFloat(nextStringNumber);
+        const newPosition =i-1;
+        newArray.splice(newPosition, 3, result);
+        i =0;
+    }
+    if(element === "*"){
+        const previousStringNumber = newArray[i -1];
+        const nextStringNumber = newArray[ i + 1];
+        const result = parseFloat(previousStringNumber) *  parseFloat(nextStringNumber);
+        const newPosition = i-1;
+        newArray.splice(newPosition, 3, result);
+        i = 0;
+    }
+}
+
+const newCopy = [...newArray];
+for (let i = 0; i < newCopy.length; i++){
+    const element = newCopy[i];
+    if (element === "+"){
+        const result = getResult(i, element)
+        const newPositionForResult = i-1;
+        newCopy.splice(newPositionForResult, 3, result);
+        i = 0;
+    }
+    if( element === '-'){
+        const result = getResult(i, element);
+        console.log(result);
+        const newPositionForResult = i - 1;
+        newCopy.splice(newPositionForResult, 3, result);
+        i = 0;
+    }
+}
+
+console.log(newCopy.toString());
+ */
